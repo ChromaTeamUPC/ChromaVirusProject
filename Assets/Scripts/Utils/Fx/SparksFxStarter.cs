9f92bd4b7d2c532f748eb72cd87cb2e578928d7d
jordisimon <jordisu@gmail.com>
2016-07-05 10:25:52 +0200

    CHG: Improved sparks fx

---
+++ Assets/Scripts/Utils/Fx/SparksFxStarter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparksFxStarter : MonoBehaviour 
+{
+    public float minStartTime = 0f;
+    public float maxStartTime = 2f;
+
+    private ParticleSystem ps;
+	// Use this for initialization
+	void Awake () 
+	{
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	void OnEnable()
+    {
+        StartCoroutine(StartPS());
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+        ps.Stop();
+    }
+
+    private IEnumerator StartPS()
+    {
+        yield return new WaitForSeconds(Random.Range(minStartTime, maxStartTime));
+
+        ps.Play();
+    }
+}
--- Assets/ZZ_ExternalResources/InControl/InControl/Source/VersionInfo.cs
+++ Assets/ZZ_ExternalResources/InControl/InControl/Source/VersionInfo.cs
@@ -28,7 +28,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 4,
-				Build = 3874
+				Build = 3882
 			};
 		}
 
