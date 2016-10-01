023d21d01ff0d2ec26f8fcbd634fc6608b12d136
jordisimon <jordisu@gmail.com>
2016-10-01 11:21:46 +0200

    CHG: Some adjustments in the intro scene

--- Assets/Scripts/Intro/NewIntroController.cs
+++ Assets/Scripts/Intro/NewIntroController.cs
@@ -60,19 +60,27 @@ public class NewIntroController : MonoBehaviour
     public GameObject alertTriangle;
     public GameObject exclamationMark;
 
+    //KDT
     public GameObject kdt;
     public SkinnedMeshRenderer kdtRenderer;
     public GameObject kdtShield;
     public Animator kdtAnimator;
+    public ParticleSystem wireframeToSolidFx;
+
+    //JNK
+    public GameObject jnk;
+    public SkinnedMeshRenderer jnkRenderer;
+    public GameObject jnkShield;
+    public Animator jnkAnimator;
+    public ParticleSystem jnkWireframeToSolidFx;
 
     public AudioSource alarmSoundFx;
     public AudioSource wireframeToSolidSoundFx;
-    public ParticleSystem wireframeToSolidFx;
 
     public GameObject skipHint;
     public FadeSceneScript fadeScript;
 
-    [Header("Materials")]
+    [Header("Materials KDT")]
     public Material wireBase;
     public Material wireColor;
     public Material wireFace;
@@ -80,7 +88,19 @@ public class NewIntroController : MonoBehaviour
     public Material matBase;
     public Material matColor;
     public Material matFace;
+    public Material matShield;
     public Color matEmissionFinalColor;
+
+    [Header("Materials JNK")]
+    public Material jnkWireBase;
+    public Material jnkWireColor;
+    public Material jnkWireFace;
+
+    public Material jnkMatBase;
+    public Material jnkMatColor;
+    public Material jnkMatFace;
+    public Material jnkMatShield;
+    public Color jnkMatEmissionFinalColor;
 
     //control variables
     private string loadingStr = "";
@@ -92,6 +112,8 @@ public class NewIntroController : MonoBehaviour
 
     private Coroutine blinkExclamation = null;
     private Coroutine rotateCircle = null;
+
+    private bool multiPlayer;
 
 	// Use this for initialization
 	void Start () 
@@ -109,6 +131,14 @@ public class NewIntroController : MonoBehaviour
         kdt.SetActive(false);
         kdtShield.SetActive(false);
 
+        mats = jnkRenderer.sharedMaterials;
+        mats[0] = jnkWireBase;
+        mats[1] = jnkWireFace;
+        mats[2] = jnkWireColor;
+        jnkRenderer.sharedMaterials = mats;
+        jnk.SetActive(false);
+        jnkShield.SetActive(false);
+
         skipHint.SetActive(false);
 
         loadingSlider.value = 0;
@@ -116,6 +146,22 @@ public class NewIntroController : MonoBehaviour
 
         percentageBackgroundText.text = "0%";
         percentageForegroundText.text = "0%";
+
+        matEmissionFinalColor = rsc.coloredObjectsMng.GetColor(ChromaColorInfo.Random);
+
+        multiPlayer = rsc.gameInfo.numberOfPlayers > 1;
+
+        if (!multiPlayer)
+        {
+            kdt.transform.position = Vector3.zero;
+        }
+        else
+        {
+            jnkMatEmissionFinalColor = rsc.coloredObjectsMng.GetColor(ChromaColorInfo.Random);
+
+            while(matEmissionFinalColor == jnkMatEmissionFinalColor)
+                jnkMatEmissionFinalColor = rsc.coloredObjectsMng.GetColor(ChromaColorInfo.Random);
+        }
 
         rsc.audioMng.FadeInMusic(AudioManager.MusicType.INTRO, 1f);
         fadeScript.StartFadingToClear(initialFadeTime);
@@ -189,6 +235,14 @@ public class NewIntroController : MonoBehaviour
 
                     kdt.SetActive(true);
 
+                    //Set jnk
+                    jnkWireBase.SetFloat("_Thickness", 0f);
+                    jnkWireColor.SetFloat("_Thickness", 0f);
+                    jnkWireFace.SetFloat("_Thickness", 0f);
+
+                    if (multiPlayer)
+                        jnk.SetActive(true);
+
                     elapsedTime = 0f;
 
                     loadingStr = "STARTING KDT PROTOCOL";
@@ -219,9 +273,15 @@ public class NewIntroController : MonoBehaviour
                         wireBase.SetFloat("_Thickness", thickness);
                         wireColor.SetFloat("_Thickness", thickness);
                         wireFace.SetFloat("_Thickness", thickness);
+
+                        jnkWireBase.SetFloat("_Thickness", thickness);
+                        jnkWireColor.SetFloat("_Thickness", thickness);
+                        jnkWireFace.SetFloat("_Thickness", thickness);
                     }
 
                     kdt.transform.Rotate(0, Time.deltaTime * startingKdtDPS, 0);
+
+                    jnk.transform.Rotate(0, Time.deltaTime * startingKdtDPS, 0);
 
                     elapsedTime += Time.deltaTime;
                 }
@@ -250,7 +310,13 @@ public class NewIntroController : MonoBehaviour
                     wireColor.SetFloat("_Thickness", thickness);
                     wireFace.SetFloat("_Thickness", thickness);
 
+                    jnkWireBase.SetFloat("_Thickness", thickness);
+                    jnkWireColor.SetFloat("_Thickness", thickness);
+                    jnkWireFace.SetFloat("_Thickness", thickness);
+
                     kdt.transform.Rotate(0, Time.deltaTime * startingKdtDPS, 0);
+
+                    jnk.transform.Rotate(0, Time.deltaTime * startingKdtDPS, 0);
 
                     elapsedTime += Time.deltaTime;
                 }
@@ -263,7 +329,15 @@ public class NewIntroController : MonoBehaviour
                     mats[2] = matFace;
                     kdtRenderer.sharedMaterials = mats;
 
+                    mats = jnkRenderer.sharedMaterials;
+                    mats[0] = jnkMatBase;
+                    mats[1] = jnkMatFace;
+                    mats[2] = jnkMatColor;
+                    jnkRenderer.sharedMaterials = mats;
+
                     wireframeToSolidFx.Play();
+                    if (multiPlayer) jnkWireframeToSolidFx.Play();
+
                     wireframeToSolidSoundFx.Play();
 
                     elapsedTime = 0f;
@@ -287,14 +361,21 @@ public class NewIntroController : MonoBehaviour
 
                     factor = (elapsedTime - activatingColorStartChangingAt) / (activatingColorDuration - activatingColorStartChangingAt);
                     matColor.SetColor("_EmissionColor", Color.Lerp(Color.black, matEmissionFinalColor, factor));
+                    jnkMatColor.SetColor("_EmissionColor", Color.Lerp(Color.black, jnkMatEmissionFinalColor, factor));
 
                     elapsedTime += Time.deltaTime;
                 }
                 else
                 {
+                    matShield.SetColor("_EmissionColor", matEmissionFinalColor);
                     kdtShield.SetActive(true);
                     kdtAnimator.SetBool("Aiming", true);
                     matColor.SetColor("_EmissionColor", matEmissionFinalColor);
+
+                    jnkMatShield.SetColor("_EmissionColor", jnkMatEmissionFinalColor);
+                    jnkShield.SetActive(true);
+                    jnkAnimator.SetBool("Aiming", true);
+                    jnkMatColor.SetColor("_EmissionColor", jnkMatEmissionFinalColor);
 
                     elapsedTime = 0;
 
@@ -316,6 +397,7 @@ public class NewIntroController : MonoBehaviour
                     percentageForegroundText.text = percentage + "%";
 
                     kdt.transform.Rotate(0, Time.deltaTime * startingProgramDPS, 0);
+                    jnk.transform.Rotate(0, Time.deltaTime * startingProgramDPS, 0);
 
                     elapsedTime += Time.deltaTime;
                 }
